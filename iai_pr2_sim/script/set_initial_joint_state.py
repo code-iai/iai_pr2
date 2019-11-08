d755218dbd2441f1a16f330d56a330f641e916bc
Simon Stelter <stelter@uni-bremen.de>
2019-11-08 13:45:59 +0100

    added small script to set the start configuration with ros control sim

---
+++ iai_pr2_sim/script/set_initial_joint_state.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import rospy
+from actionlib import ActionClient
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+rospy.init_node('initial_joint_state_setter',
+                anonymous=True)
+ac = ActionClient('/whole_body_controller/body/follow_joint_trajectory', FollowJointTrajectoryAction)
+ac.wait_for_server()
+goal = FollowJointTrajectoryGoal()
+traj = JointTrajectory()
+traj.header.stamp = rospy.get_rostime()
+traj.joint_names = ['torso_lift_joint',
+                    'r_upper_arm_roll_joint',
+                    'r_shoulder_pan_joint',
+                    'r_shoulder_lift_joint',
+                    'r_forearm_roll_joint',
+                    'r_elbow_flex_joint',
+                    'r_wrist_flex_joint',
+                    'r_wrist_roll_joint',
+                    'l_upper_arm_roll_joint',
+                    'l_shoulder_pan_joint',
+                    'l_shoulder_lift_joint',
+                    'l_forearm_roll_joint',
+                    'l_elbow_flex_joint',
+                    'l_wrist_flex_joint',
+                    'l_wrist_roll_joint',
+                    'head_pan_joint',
+                    'head_tilt_joint']
+
+p = JointTrajectoryPoint()
+p.positions = [0.1687062500000004,
+               0,
+               0,
+               0,
+               0,
+               -1.1400000000000001,
+               -1.0499999999999998,
+               0,
+               0,
+               0,
+               0,
+               0,
+               -1.1399999999999995,
+               -1.0499999999999998,
+               0,
+               0,
+               0]
+p.velocities = [0] * len(p.positions)
+p.time_from_start = rospy.Duration(1)
+traj.points.append(p)
+goal.trajectory = traj
+ac.send_goal(goal)
